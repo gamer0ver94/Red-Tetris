1d5228e8c95968b5d07125dbefd484b6f211896d
Duarte Paulino <dpaulino@k1r2p1.42mulhouse.fr>
2026-04-28 13:42:01 +0200

    clean branch

--- src/client/components/Form.tsx
+++ src/client/components/Form.tsx
@@ -1,46 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Form.css";
 
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { setUsername } from "../store/userSlice";
+import { fetchData } from "./fetch/fetch";
+import { socket } from "../socket/socket";
 
 export default function Form() {
   const goTo = useNavigate();
-
   const dispatch = useAppDispatch();
-  const username = useAppSelector((state) => state.user.username);
 
   const [input, setInput] = useState("");
 
   async function confirm() {
-    try {
-      const res = await fetch("http://localhost:1800/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          username: input
-        })
-      });
-
-      if (!res.ok) {
-        throw new Error("Request failed");
-      }
-
-      const data = await res.json();
-      console.log(data);
-
-      // save to redux
-      dispatch(setUsername(data.username));
-
+    const payload = {
+      username: input
+    }
+    const data = await fetchData("http://localhost:1800/auth/register", payload, "POST");
+    console.log(data);
+    dispatch(setUsername(input));
+    if (data.username) {
       goTo("/join");
-
-    } catch (err) {
-      console.error(err);
     }
   }
+
+  useEffect(() => {
+    socket.connect();
+  },[]);
 
   return (
     <div>
--- src/client/pages/Join.tsx
+++ src/client/pages/Join.tsx
@@ -1,30 +1,67 @@
 import { useNavigate } from "react-router-dom"
-import { useAppSelector } from "../hooks/reduxHooks"
-import { io } from "socket.io-client"
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks"
+import { useEffect, useState } from "react"
+import { fetchData } from "../components/fetch/fetch"
+import { setUsername } from "../store/userSlice";
+import { socket } from "../socket/socket";
 export default function Join() {
     const goTo = useNavigate()
     const username = useAppSelector((state) => state.user.username)
-    function confirm(route:string) {
-        goTo(route)
+    const [playerMode, setPlayerMode] = useState("single_player")
+    const [gameMode, setGameMode] = useState("classic")
+    const dispatch = useAppDispatch();
+    async function confirm(route:string){
+        const data = await fetchData("http://localhost:1800/auth/register", {
+            username: username,
+            playerMode: playerMode,
+            gameMode: gameMode
+        }, "POST");
+        console.log(data);
+        if (data.username) {
+            goTo(route);
+        }
     }
+
+    useEffect(() => {
+        if (username) {
+            return;
+        }
+        async function loadUser() {
+            const data = await fetchData("http://localhost:1800/auth/me", null,"GET");
+            dispatch(setUsername(data.username));
+            socket.auth = { csrf_token: data.csrf_token };
+        }
+        loadUser();
+        socket.connect();
+        socket.on("connect", () => {
+    console.log("connected");
+    // Now you can emit lobby events
+});
+    }, []);
+
     return (
         <div>
             <div>
-                <h1>{username ? username : "No Player"}</h1>
+                <h1>Welcome, {username ? username : "No Player"}</h1>
             </div>
             <div>
-                <h1>Mode</h1>
-                <h1>Default</h1>
+                <h1>{playerMode}</h1>
+                <h1>{gameMode}</h1>
             </div>
             <div>
                 <div>
-                    <h1>SinglePlayer</h1>
-                </div>
-                <div>
-                    <h1>MultiPlayer</h1>
-                </div>
+                    <h1>Player Mode</h1>
+                    <select value={playerMode} onChange={(e)=>setPlayerMode(e.target.value)}>
+                        <option value="single_player">Single Player</option>
+                        <option value="multiplayer">Multiplayer</option>
+                    </select>
                 <div>
                     <h1>Mode</h1>
+                    <select  value={gameMode} onChange={(e)=>setGameMode(e.target.value)}>
+                        <option value="classic">Classic</option>
+                        <option value="Boost Mode">Boost Mode</option>
+                    </select>
+                </div>
                 </div>
                 <div>
                     <button onClick={()=>confirm("/lobby")}>Create</button>
